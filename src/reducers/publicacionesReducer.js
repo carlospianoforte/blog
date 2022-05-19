78a16c862d9a1d85d9707c373de3ca6550be19b7
Carlos Ortiz <carlospianiforte@gmail.com>
2022-05-19 01:47:21 -0500

    publicaciones

--- src/reducers/publicacionesReducer.js
+++ src/reducers/publicacionesReducer.js
@@ -1,9 +1,15 @@
-import {ACTUALIZAR, LOADING, ERROR} from '../types/publicacionesTypes';
+import {ACTUALIZAR, 
+    LOADING, ERROR, 
+    COM_ERROR, 
+    COM_LOADING, 
+    COM_ACTUALIZAR} from '../types/publicacionesTypes';
 
 const INITIAL_STATE = {
     publicaciones: [],
     loading: false,
     error: '',
+    com_loading: false,
+    com_error: '',
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -25,6 +31,23 @@ export default (state = INITIAL_STATE, action) => {
                     error: action.payload,
                     loading: false
                 }
+
+                case COM_ACTUALIZAR:
+                    return {
+                        ...state,
+                        publicaciones: action.payload,
+                        com_loading: false,
+                        com_error: '',
+                    };
+
+                case COM_LOADING:
+                    return {...state, com_loading: true}
+        
+                case COM_ERROR:
+                    return {...state, 
+                        com_error: action.payload,
+                        com_loading: false
+                    }
 
         default: return state;
     };
