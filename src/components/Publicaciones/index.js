af19f38a4419092d13aae85198ffa8ae6889dff3
Carlos Ortiz <carlospianiforte@gmail.com>
2022-05-31 02:25:16 -0500

    firebase3

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -36,6 +36,7 @@ const Publicaciones = (props) => {
                 }
             })();
             
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         },[]);
 
     const ponerUsuario = () => {
